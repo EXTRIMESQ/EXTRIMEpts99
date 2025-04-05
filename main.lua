c332ca1bd3b0eb4af1e12187e57537607b7465e9
EXTRIMESQ <vladislavponifly@gmail.com>
2025-04-05 16:12:17 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+-- Загружаем меню и автопрокрутку
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EXTRIMESQ/EXTRIMEpts99/main/menu.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EXTRIMESQ/EXTRIMEpts99/main/autoegg.lua"))()
